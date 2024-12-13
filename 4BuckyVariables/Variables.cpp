2d66f0c69f94e9727df4fc1d846e0ca647bf1084
Oppie1 <atindall729@gmail.com>
2024-12-13 16:09:51 -0600

    Add files via upload

---
+++ 4BuckyVariables/Variables.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+
+using namespace std;
+
+int main()
+
+{
+	int a = 4;  // first number "int" = integar (a)
+	int b = 21; // second number "int" integar (b)
+
+	int sum = a - b;
+
+	cout << sum;
+
+	return 0;
+
+
+}
